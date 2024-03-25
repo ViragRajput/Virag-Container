a955c7bf73aa7c4d645aef41c2da00a8d7ca7dcb
ViragRajput <108286928+ViragRajput@users.noreply.github.com>
2024-03-25 10:41:18 +0530

    refactor comments & examples docs

--- src/Container.php
+++ src/Container.php
@@ -3,8 +3,6 @@
 /*
  * This file is part of the ViragContainer package.
  *
- * (c) Virag Rajput <codewithvirag@gmail.com>
- *
  * ViragContainer is a lightweight PHP dependency injection container designed to manage object 
  * creation and resolution.
  * It provides a flexible and powerful way to manage dependencies in your PHP projects.
